c8dd0dae229233517631b7ab0cc095b0eec18613
Imad <baddin2016@gmail.com>
2017-08-20 01:20:15 +0100

    editing the media.py for the new changes in the entertainment_center.py

--- media.py
+++ media.py
@@ -1,11 +1,22 @@
 '''The movie class'''
 
-
 class Movie():
     '''The movie class constructor with parameters the title,
     description, image, and movie trailer'''
-    def __init__(self, title, movie_storyline, poster_image, trailer_youtube):
-        self.title = title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+    def __init__(self, MovieTrailer):
+        self.title = MovieTrailer.title
+        self.storyline = MovieTrailer.description
+        self.poster_image_url = MovieTrailer.image
+        self.trailer_youtube_url = MovieTrailer.video
+
+
+class MovieTrailer:
+	def __init__(self, title, description, image, video):
+		self.title = title
+		self.description = description
+		self.image = image
+		self.video = video
+
+	def get_media(self):
+		return Movie(self)
+
