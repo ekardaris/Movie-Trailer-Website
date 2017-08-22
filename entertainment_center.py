808459ada9247a582a68eb2b43b5f5a49ee58dff
Emmanouil Kardaris <emmanouilkardaris@gmail.com>
2017-08-23 00:58:20 +0300

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -55,3 +55,5 @@ fresh_tomatoes.open_movies_page(movies)
 '''TO DO: Commit to github'''
 '''TO DO: Commit to github'''
 '''TO DO: Commit to github'''
+'''TO DO: Commit to github'''
+'''TO DO: Commit to github'''
