b5adade8f9419f8ed611040f8105afa4ac825243
Emmanouil Kardaris <emmanouilkardaris@gmail.com>
2017-09-03 01:37:25 +0300

    fix: Remove unnecessary functionality
    In this implementation we clear the code and remove unnecessary functionality.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,3 +1,50 @@
-'''
-TO BE REPLACED WITH THE ACTUAL CODE
-'''
+import media
+import fresh_tomatoes
+
+# Avatar movie
+title = "Avatar"
+description = "A marine on an alien planet"
+image = "https://upload.wikimedia.org/wikipedia"
+image2 = image + "/en/b/b0/Avatar-Teaser-Poster.jpg"
+video = "https://www.youtube.com/watch?v=5PSNL1qE6VY"
+avatar = media.Movie(title, description, image2, video)
+
+# School of rock movie
+title = "School of rock"
+description = "Using rock music to learn"
+image = "https://upload.wikimedia.org/wikipedia"
+image2 = image + "/en/1/11/School_of_Rock_Poster.jpg"
+video = "https://www.youtube.com/watch?v=XCwy6lW5Ixc"
+
+school_of_rock = media.Movie(title, "Using rock music to learn", image2, video)
+
+# Ratatouille movie
+title = "Ratatouille"
+description = "A rat is a chef in Paris"
+image = "https://upload.wikimedia.org/wikipedia/"
+image2 = image+"/en/5/50/RatatouillePoster.jpg"
+video = "https://www.youtube.com/watch?v=c3sBBRxDAqk"
+ratatouille = media.Movie(title, description, image2, video)
+
+# Midnight in paris movie
+title = "Midnight in Paris"
+description = "Going back in time to meet authors"
+image = "https://upload.wikimedia.org/wikipedia"
+image2 = image+"/en/9/9f/Midnight_in_Paris_Poster.jpg"
+video = "https://www.youtube.com/watch?v=FAfR8omt-CY"
+midnight_in_paris = media.Movie(title, description, image2, video)
+
+# Hunger games movie
+
+title = "Hunger games"
+description = "A really real reality show"
+image = "https://upload.wikimedia.org/wikipedia"
+image2 = image + "/en/3/39/The_Hunger_Games_cover.jpg"
+video = "https://www.youtube.com/watch?v=mfmrPu43DF8"
+hunger_games = media.Movie(title, description, image2, video)
+
+# The movie list to show
+movies = [avatar, school_of_rock, ratatouille, midnight_in_paris, hunger_games]
+
+# Open the movies page
+fresh_tomatoes.open_movies_page(movies)
